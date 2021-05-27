aa2a2edcc801462678ddfe3c6f93bd38daae3b2a
unknown <selvavignesh128@gmail.com>
2021-05-28 01:27:23 +0530

    Add Mongo config

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-//Route files
-const bootcamps = require('./routes/bootcamps');
+const connectDB = require('./config/db');
 
 //load env vars
 dotenv.config({path: './config/config.env'});
+
+//Connect to database
+connectDB();
+
+//Route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -20,5 +25,12 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`))
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`))
+
+//Handle unhandle promise rejections
+process.on('unhandledRejection', (err, promise) =>{
+    console.log(`Error: ${err.message}`);
+    //Close Server & exit process
+    server.close(()=> process.exit(1));
+})
 
