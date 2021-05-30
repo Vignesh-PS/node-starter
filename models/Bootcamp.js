8cb25b896f1ccfc2528307f4d55accc058c0cada
unknown <selvavignesh128@gmail.com>
2021-05-31 00:53:59 +0530

    Connect Mongodb bootcamp controller

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -28,7 +28,7 @@ const BootcampSchema = new mongoose.Schema({
         type: {
             type: String, // Don't do `{ location: { type: String } }`
             enum: ['Point'], // 'location.type' must be 'Point'
-            required: true
+           // required: true
         },
         coordinates: {
             type: [Number],
