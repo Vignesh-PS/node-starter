cd3973b6c6c3263768851bdba9ddcd9e05f4077d
unknown <selvavignesh128@gmail.com>
2021-06-04 23:31:58 +0530

    Adding exception handler middleware

---
+++ middleware/error.js
@@ -0,0 +1,30 @@
+const ErrorHandler = require('../utils/errorReponse');
+
+const errorHandler = (err, req, res, next)=>{
+        console.log(err.stack);
+        let error = {...err};
+
+        console.log('error :>> ', error);
+        
+        //Mongoose Invalid Id
+        if(err.name=='CastError'){
+            const message = `Bootcamp not found with id of ${err.value}`;
+            error = new ErrorHandler(message, 404);
+        }
+
+        //Mongoose duplicate key
+        if(err.code===11000){
+            const message = `Duplicates fields entered`;
+            error = new ErrorHandler(message, 400);
+        }
+
+        if(err.name=='ValidationError'){
+            const message = Object.values(err.errors).map(val => val.message);
+            error = new ErrorHandler(message, 400)
+        }
+        
+        
+        res.status(error.statusCode || 500).json({success: false, error: error.message || 'Server error'});
+}
+
+module.exports = errorHandler;
